b2188e2d833c2da0f84026e1f3370f542afb0811
karol <karlostopchiev@gmail.com>
2020-09-26 15:44:56 +0300

    arrayrotation

---
+++ Arrays/ArrayRotation/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+
+namespace ArrayRotation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int[] topIntegers = new int[nums.Length];
+            int checkTrue = 0;
+            int counter = 0;
+            int zeroCount = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (i < nums.Length)
+                {
+                    for (int j = i; j < nums.Length; j++)
+                    {
+                        if (nums[i] > nums[j])
+                        {
+                            checkTrue++;
+                        }
+                    }   
+                }
+
+                if (checkTrue == nums.Length - 1 - i)
+                {
+                    topIntegers[counter] = nums[i];
+                    counter++;
+                    checkTrue = 0;
+                }
+
+                checkTrue = 0;
+            }
+
+            int[] resultForPrintArr = new int[counter];
+
+            for (int i = 0; i < resultForPrintArr.Length; i++)
+            {
+                resultForPrintArr[i] = topIntegers[i];
+            }
+
+            foreach (var digit in resultForPrintArr)
+            {
+                Console.Write($"{digit} ");
+            }
+        }
+    }
+}
