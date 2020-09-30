0f43c64a6e6719cca332602c619b61807c45c89d
karol <karlostopchiev@gmail.com>
2020-09-30 13:29:41 +0300

    MaxSeqOfEqualEl

--- Arrays/MaxSequenceOfEqualElements/Program.cs
+++ Arrays/MaxSequenceOfEqualElements/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data.SqlTypes;
 using System.Linq;
 
 namespace MaxSequenceOfEqualElements
@@ -7,8 +8,35 @@ namespace MaxSequenceOfEqualElements
     {
         static void Main(string[] args)
         {
-            int[] sequence = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
-            
+            int[] mainSequence = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int previousSeqLen = 1;
+            int currentSeqLen = 1;
+            int previousNum = 0;
+
+            for (int i = 0; i < mainSequence.Length; i++)
+            {
+                if (i < mainSequence.Length - 1)
+                {
+                    if (mainSequence[i] == mainSequence[i + 1])
+                    {
+                        currentSeqLen++;
+                        if (currentSeqLen > previousSeqLen)
+                        {
+                            previousNum = mainSequence[i];
+                            previousSeqLen = currentSeqLen;
+                        }
+                    }
+                    else
+                    {
+                        currentSeqLen = 1;
+                    }
+                }
+            }
+
+            for (int i = 0; i < previousSeqLen; i++)
+            {
+                Console.Write($"{previousNum} ");
+            }
         }
     }
 }
