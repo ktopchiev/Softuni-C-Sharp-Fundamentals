8d0c400d9a6c9b6538755566b789a7c011e72d86
karol <karlostopchiev@gmail.com>
2020-09-30 13:55:00 +0300

    MagicSum

---
+++ Arrays/MagicSum/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace MagicSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] intArray = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int num = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < intArray.Length; i++)
+            {
+                for (int j = i + 1; j < intArray.Length; j++)
+                {
+                    if (intArray[i] + intArray[j] == num)
+                    {
+                        Console.Write($"{intArray[i]} {intArray[j]}");
+                        Console.WriteLine();
+                    }
+                }
+            }
+        }
+    }
+}
