ce0545403ac59aca81b59cda6c174db2e51c96a2
karol <karlostopchiev@gmail.com>
2020-10-01 01:22:17 +0300

    SmallestNum

---
+++ Methods/SmallestOfThreeNumbers/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace SmallestOfThreeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+            int thirdNum = int.Parse(Console.ReadLine());
+
+            int smallestOfThreeNums = FindSmallestNum(firstNum, secondNum, thirdNum);
+            Console.WriteLine(smallestOfThreeNums);
+        }
+
+        static int FindSmallestNum(int firstNum, int secondNum, int thirdNum)
+        {
+            if (firstNum < secondNum && firstNum < thirdNum)
+            {
+                return firstNum;
+            }
+            
+            else if (secondNum < firstNum && secondNum < thirdNum)
+            {
+                return secondNum;
+            }
+
+            else
+            {
+                return thirdNum;
+            }
+        }
+    }
+}
