3755e27bcbd5fd02b6d66289a3b398b37b5539e0
karol <karlostopchiev@gmail.com>
2020-10-01 10:59:49 +0300

    vowels

---
+++ Methods/VowelsCount/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace VowelsCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] symbols = Console.ReadLine().ToLower().ToCharArray();
+            int vowelsNum = GetVowelsNumber(symbols);
+            Console.WriteLine(vowelsNum);
+        }
+
+        static int GetVowelsNumber(char[] symbols)
+        {
+            char[] vowelsList = {'a', 'e', 'i', 'o', 'u', 'y' };
+            int vowelsCounter = 0;
+            
+            for (int symbol = 0; symbol < symbols.Length; symbol++)
+            {
+                for (int vowel = 0; vowel < vowelsList.Length; vowel++)
+                {
+                    if (symbols[symbol] == vowelsList[vowel])
+                    {
+                        vowelsCounter++;
+                    }
+                }
+            }
+
+            return vowelsCounter;
+        }
+    }
+}
