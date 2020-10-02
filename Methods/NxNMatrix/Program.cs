8847aa857768359a4402df7ec05d17cbf2c6ad65
karol <karlostopchiev@gmail.com>
2020-10-02 12:06:58 +0300

    NxNMatrix

---
+++ Methods/NxNMatrix/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+
+namespace NxNMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine(PrintNxNMatrix(num));
+        }
+
+        static string PrintNxNMatrix(int num)
+        {
+           StringBuilder s = new StringBuilder();
+
+           for (int i = 0; i < num; i++)
+           {
+               for (int j = 0; j < num; j++)
+               {
+                   s.Append(num.ToString() + " ");   
+               }
+
+               s.Append(System.Environment.NewLine);
+           }
+
+           return s.ToString();
+        }
+    }
+}
