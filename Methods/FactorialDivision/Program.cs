9cea46f008e718816c790a3434c3b397a0e3447a
karol <karlostopchiev@gmail.com>
2020-10-02 12:43:50 +0300

    FactorialDivision

---
+++ Methods/FactorialDivision/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace FactorialDivision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstNum = double.Parse(Console.ReadLine());
+            var secondNum = double.Parse(Console.ReadLine());
+
+            var factorial = Factorial(firstNum);
+            var secondFactorial = Factorial(secondNum);
+            Console.WriteLine($"{Divisor(factorial, secondFactorial):F}");
+        }
+
+        static double Factorial(double num)
+        {
+            double factorial = 1;
+            
+            for (double i = 1; i <= Math.Abs(num); i++)
+            {
+                factorial *= i;
+            }
+
+            return factorial;
+        }
+
+        static double Divisor(double num, double secondNum)
+        {
+            return num / secondNum;
+        }
+    }
+}
