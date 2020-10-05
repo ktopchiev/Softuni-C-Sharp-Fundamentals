2597872e4d82f51ad48a9ee5a216e1d5fabe7f4e
karol <karlostopchiev@gmail.com>
2020-10-05 20:35:31 +0300

    HouseParty

--- Lists/HouseParty/Program.cs
+++ Lists/HouseParty/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace HouseParty
 {
@@ -6,7 +8,40 @@ namespace HouseParty
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int guestsNum = int.Parse(Console.ReadLine());
+            List<string> guestList = new List<string>();
+            
+            for (int i = 0; i < guestsNum; i++)
+            {
+                string[] commandInput = Console.ReadLine().Split(' ').ToArray();
+                string name = commandInput[0];
+
+                if (commandInput[2] != "not")
+                {
+                    if (guestList.Exists(item => item == name))
+                    {
+                        Console.WriteLine($"{name} is already in the list!");
+                    }
+                    else
+                    {
+                        guestList.Add(name);
+                    }
+                }
+                else
+                {
+                    if(!guestList.Exists(item => item == name))
+                    {
+                        Console.WriteLine($"{name} is not in the list!");
+                    }
+                    
+                    guestList.Remove(name);
+                }
+            }
+
+            foreach (var guest in guestList)
+            {
+                Console.WriteLine(guest);
+            }
         }
     }
 }
