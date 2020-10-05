06dc8ff5baa32b076a3dceb428d9259207b7ffdd
karol <karlostopchiev@gmail.com>
2020-10-05 19:05:00 +0300

    TopIntegers

--- Arrays/TopIntegers/Program.cs
+++ Arrays/TopIntegers/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Linq;
 
-namespace ArrayRotation
+namespace TopIntegers
 {
     class Program
     {
--- Lists/Train/Program.cs
+++ Lists/Train/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Train
 {
@@ -6,7 +8,44 @@ namespace Train
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<int> wagons = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            int capacity = int.Parse(Console.ReadLine());
+            bool flag = true;
+
+            while (flag)
+            {
+                string[] inputArray = Console.ReadLine().Split(' ').ToArray();
+
+                for (int i = 0; i < inputArray.Length; i++)
+                {
+                    if (inputArray[0] == "Add")
+                    {
+                        wagons.Add(int.Parse(inputArray[1]));
+                        break;
+                    }
+                    
+                    else if (inputArray[0] == "end")
+                    {
+                        flag = false;
+                        break;
+                    }
+
+                    else
+                    {
+                        foreach (var wagon in wagons)
+                        {
+                            if (wagon + int.Parse(inputArray[0]) <= capacity)
+                            {
+                                int newData = wagon + int.Parse(inputArray[0]);
+                                wagons[wagons.FindIndex(ind=>ind.Equals(wagon))] =  newData;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+            
+            Console.WriteLine(String.Join(" ", wagons));
         }
     }
 }
