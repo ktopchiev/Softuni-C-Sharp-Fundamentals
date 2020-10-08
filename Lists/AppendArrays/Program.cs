1b50cf1865791720d19525613497d5b6b0b089f5
karol <karlostopchiev@gmail.com>
2020-10-08 23:46:53 +0300

    AppendArrays

--- Lists/AppendArrays/Program.cs
+++ Lists/AppendArrays/Program.cs
@@ -13,23 +13,23 @@ namespace AppendArrays
             List<string> input = Console.ReadLine().Split(new string[] {"|"}, StringSplitOptions.None).ToList();
             List<string> outputList = new List<string>();
             
-                foreach (var element in input)
+            foreach (var element in input)
+            {
+                string s = element;
+                
+                //Remove where whitespaces between element are more than one
+                RegexOptions options = RegexOptions.None;
+                Regex regex = new Regex("[ ]{2,}", options);
+                s = regex.Replace(s, " ");
+                
+                s = s.Trim();
+                
+                //Insert the element at the beginning of the list
+                if (!s.Equals("") && !s.Equals(" ")) 
                 {
-                    string s = element;
-                    
-                    //Remove where whitespaces between element are more than one
-                    RegexOptions options = RegexOptions.None;
-                    Regex regex = new Regex("[ ]{2,}", options);
-                    s = regex.Replace(s, " ");
-                    
-                    s = s.Trim();
-                    
-                    //Insert the element at the beginning of the list
-                    if (!s.Equals("") && !s.Equals(" ")) 
-                    {
-                       outputList.Insert(0, s);
-                    }
+                   outputList.Insert(0, s);
                 }
+            }
                 
             Console.WriteLine(string.Join(" ", outputList));
         }
