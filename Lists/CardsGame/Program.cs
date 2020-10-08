652435cd0625ef3e34718f21883c8bd631b8ac13
karol <karlostopchiev@gmail.com>
2020-10-08 19:23:04 +0300

    CardsGame

--- Lists/CardsGame/Program.cs
+++ Lists/CardsGame/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace CardsGame
 {
@@ -6,7 +8,55 @@ namespace CardsGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<int> playerOne = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            List<int> playerTwo = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+            for (int i = 0; i >= 0 ; i++)
+            {
+                if (playerOne.Count > 0 && playerTwo.Count > 0)
+                {
+                    if (playerOne[i] > playerTwo[i])
+                    {
+                        int add = playerOne[i];
+                        playerOne.Remove(playerOne[i]);
+                        playerOne.Add(add);
+                        playerOne.Add(playerTwo[i]);
+                        playerTwo.Remove(playerTwo[i]);
+                        i--;
+                    }
+                    
+                    else if (playerOne[i] < playerTwo[i])
+                    {
+                        int add = playerTwo[i];
+                        playerTwo.Remove(playerTwo[i]);
+                        playerTwo.Add(add);
+                        playerTwo.Add(playerOne[i]);
+                        playerOne.Remove(playerOne[i]);
+                        i--;
+                    }
+                    
+                    else
+                    {
+                        playerOne.Remove(playerOne[i]);
+                        playerTwo.Remove(playerTwo[i]);
+                        i--;
+                    }
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            if (playerOne.Count != 0)
+            {
+                Console.WriteLine($"First player wins! Sum: {playerOne.Sum()}");
+            }
+            else if (playerTwo.Count != 0)
+            {
+                Console.WriteLine($"Second player wins! Sum: {playerTwo.Sum()}");
+            }
+            
         }
     }
 }
