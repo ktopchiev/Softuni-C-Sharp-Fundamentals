0f67ef9fc43721ce9534d11d73772b1bc6af2f59
karol <karlostopchiev@gmail.com>
2020-10-11 16:26:58 +0300

    Exam

---
+++ Basic Syntax Conditional Statements and Loops - Exercise/RageExpenses/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RageExpenses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lostGamesCount = int.Parse(Console.ReadLine());
+            float headsetPrice = float.Parse(Console.ReadLine());
+            float mousePrice = float.Parse(Console.ReadLine());
+            float keyboardPrice = float.Parse(Console.ReadLine());
+            float displayPrice = float.Parse(Console.ReadLine());
+
+            int trashedHeadset = 0;
+            int trashedMouses = 0;
+            int trashedKeyboards = 0;
+            int trashedDisplays = 0;
+            float expenses = 0;
+            bool headsetWasTrashed = false;
+
+            for (int i = 1; i <= lostGamesCount; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    trashedHeadset++;
+                    headsetWasTrashed = true;
+                }
+
+                if (i % 3 == 0)
+                {
+                    trashedMouses++;
+                    
+                    if (headsetWasTrashed)
+                    {
+                        trashedKeyboards++;
+                        
+                        if (trashedKeyboards % 2 == 0)
+                        {
+                            trashedDisplays = trashedKeyboards / 2;
+                        }
+                    }
+                }
+
+                headsetWasTrashed = false;
+            }
+            
+            // Dictionary<string, int> trashedEquipmentCounts = new Dictionary<string, int>()
+            // {
+            //     {"headset", trashedHeadset},
+            //     {"mouses", trashedMouses},
+            //     {"keyboards", trashedKeyboards},
+            //     {"displays", trashedDisplays}
+            // };
+            expenses = headsetPrice * trashedHeadset + mousePrice * trashedMouses + keyboardPrice * trashedKeyboards +
+                       displayPrice * trashedDisplays;
+
+            Console.WriteLine($"Rage expenses: {expenses:F} lv.");
+
+            // foreach (KeyValuePair<string,int> item in trashedEquipmentCounts)
+            // {
+            //     Console.WriteLine(item);
+            // }
+            
+            
+
+        }
+    }
+}
---
+++ Programming Fundamentals Mid Exam - 5 July 2020/SoftUniReception/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SoftUniReception
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstEmployeeEff = int.Parse(Console.ReadLine());
+            int secondEmployeeEff = int.Parse(Console.ReadLine());
+            int thirdEmployeeEff = int.Parse(Console.ReadLine());
+            int studentsCount = int.Parse(Console.ReadLine());
+            int hours = 0;
+            int timeEfficiencySum = firstEmployeeEff + secondEmployeeEff + thirdEmployeeEff;
+            int limit = studentsCount / timeEfficiencySum;
+            
+            for (int i = 1; i <= limit + 1; i++)
+            {
+                studentsCount -= timeEfficiencySum;
+                hours++;
+                if (i % 4 == 0)
+                {
+                    hours++;
+                }
+            }
+
+            Console.WriteLine($"Time needed: {hours}h.");
+        }
+    }
+}
