373395600caa4d2af873d84b786743b0c072c436
ktopchiev <karlostopchiev@gmail.com>
2020-10-28 13:26:39 +0200

    MuOnline 70% with wrong tests may be

--- Programming Fundamentals Mid Exam - 29 February 2020 Group 1/MuOnline/Program.cs
+++ Programming Fundamentals Mid Exam - 29 February 2020 Group 1/MuOnline/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace MuOnline
 {
@@ -6,7 +7,79 @@ namespace MuOnline
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int health = 100;
+            int bitcoins = 0;
+            int value = 0;
+            int roomCounter = 0;
+            
+            string[] input = Console.ReadLine().Split('|').ToArray();
+            
+            
+            foreach (var room in input)
+            {
+                string num = String.Empty;
+                string command = String.Empty;
+                roomCounter++;
+                
+                for (int i = 0; i < room.Length; i++)
+                {
+                    if (Char.IsDigit(room[i]))
+                    {
+                        num += room[i];
+                    }
+
+                    else
+                    {
+                        command += room[i];
+                    }
+                }
+                
+                if (num.Length > 0)
+                {
+                    value = int.Parse(num);
+                }
+
+                command = command.Trim();
+                
+                if (command == "potion")
+                {
+                    health += value;
+                    if (health > 100)
+                    {
+                        health = 100;
+                    }
+
+                    Console.WriteLine($"You healed for {value} hp.");
+                    Console.WriteLine($"Current health: {health} hp.");
+                }
+                else if (command == "chest")
+                {
+                    bitcoins += value;
+                    Console.WriteLine($"You found {value} bitcoins.");
+                }
+                else
+                {
+                    health -= value;
+                    
+                    if (health > 0)
+                    {
+                        Console.WriteLine($"You slayed {command}.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"You died! Killed by {command}.");
+                        Console.WriteLine($"Best room: {roomCounter}");
+                        break;
+                    }
+                }
+            }
+
+            if (health > 0)
+            {
+                Console.WriteLine("You've made it!");
+                Console.WriteLine($"Bitcoins: {bitcoins}");
+                Console.WriteLine($"Health: {health}");
+            }
         }
     }
 }
