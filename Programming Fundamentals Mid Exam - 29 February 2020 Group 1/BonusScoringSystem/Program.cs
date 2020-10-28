01c455b8bf7f2b4c1454032496b352b68d6be602
ktopchiev <karlostopchiev@gmail.com>
2020-10-28 12:10:11 +0200

    BonusScoringSystem

--- Programming Fundamentals Mid Exam - 29 February 2020 Group 1/BonusScoringSystem/Program.cs
+++ Programming Fundamentals Mid Exam - 29 February 2020 Group 1/BonusScoringSystem/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace BonusScoringSystem
 {
@@ -6,7 +7,26 @@ namespace BonusScoringSystem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double studentsCount = double.Parse(Console.ReadLine());
+            double lecturesCount = double.Parse(Console.ReadLine());
+            double initialBonus = double.Parse(Console.ReadLine());
+            double attendance = 0;
+            double[] bonusesArray = new double[(int)studentsCount];
+            double[] lecturesArray = new double[(int)studentsCount];
+            double totalBonus;
+            
+            for (int i = 0; i < studentsCount; i++)
+            {
+                attendance = int.Parse(Console.ReadLine());
+                totalBonus = (attendance / lecturesCount) * (5 + initialBonus);
+                bonusesArray[i] = totalBonus;
+                lecturesArray[i] = attendance;
+            }
+            
+            double maxBonus = bonusesArray.Max();
+            double maxAttendance = lecturesArray.Max();
+            Console.WriteLine($"Max Bonus: {Math.Round(maxBonus)}.");
+            Console.WriteLine($"The student has attended {maxAttendance} lectures.");
         }
     }
 }
