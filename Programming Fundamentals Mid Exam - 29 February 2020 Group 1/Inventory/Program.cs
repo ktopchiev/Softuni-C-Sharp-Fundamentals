9cf8bb9d8f87d2e0db0d1702a4244f1fafc94384
ktopchiev <karlostopchiev@gmail.com>
2020-11-01 20:16:31 +0200

    Inventory

--- Programming Fundamentals Mid Exam - 29 February 2020 Group 1/Inventory/Program.cs
+++ Programming Fundamentals Mid Exam - 29 February 2020 Group 1/Inventory/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Inventory
 {
@@ -6,7 +8,58 @@ namespace Inventory
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<string> inventory = Console.ReadLine().Split(", ").ToList();
+
+            while (true)
+            {
+                string[] commands = Console.ReadLine().Split(" - ").ToArray();
+                
+                if (commands.Contains("Craft!"))
+                {
+                    break;
+                }
+                
+                string item = commands[1];
+
+                if (commands.Contains("Collect"))
+                {
+                    if (inventory.Contains(item))
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        inventory.Add(item);
+                    }
+                }
+                else if (commands.Contains("Drop"))
+                {
+                    if (inventory.Contains(item))
+                    {
+                        inventory.Remove(item);
+                    }
+                }
+                else if (commands.Contains("Combine Items"))
+                {
+                   var arr = item.Split(':');
+                   
+                    if (inventory.Contains(arr[0]))
+                    {
+                        int itemIndex = inventory.IndexOf(arr[0]);
+                        inventory.Insert(itemIndex + 1, arr[1]);
+                    }
+                }
+                else if (commands.Contains("Renew"))
+                {
+                    if (inventory.Contains(item))
+                    {
+                        inventory.Remove(item);
+                        inventory.Add(item);
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", inventory));
         }
     }
 }
