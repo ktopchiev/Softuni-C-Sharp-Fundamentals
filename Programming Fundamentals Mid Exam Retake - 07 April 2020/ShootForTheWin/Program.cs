c2b516970dd3d02bad58614355e4a105917c3694
ktopchiev <karlostopchiev@gmail.com>
2020-11-05 19:23:39 +0200

    new

--- Programming Fundamentals Mid Exam Retake - 07 April 2020/ShootForTheWin/Program.cs
+++ Programming Fundamentals Mid Exam Retake - 07 April 2020/ShootForTheWin/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ShootForTheWin
 {
@@ -6,7 +8,50 @@ namespace ShootForTheWin
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] targetSequence = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int shotTargets = 0;
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (!input.Contains("End"))
+                {
+                   int targetIndex = Convert.ToInt32(input);
+                   int index = 0;
+
+                   if (targetIndex >= targetSequence.Length)
+                   {
+                       continue;
+                   }
+
+                   var lastShotTarget = targetSequence[targetIndex];
+                   
+                   targetSequence[targetIndex] = -1;
+                   shotTargets++;
+                   
+                   for (int i = 0; i < targetSequence.Length; i++)
+                   {
+                       
+                       if (targetSequence[i] != -1)
+                       {
+                           if (targetSequence[i] > lastShotTarget)
+                           {
+                               targetSequence[i] -= lastShotTarget;
+                           }
+                           else
+                           {
+                               targetSequence[i] += lastShotTarget;
+                           }
+                       }
+                   }
+                }
+                else
+                {
+                    Console.WriteLine($"Shot targets: {shotTargets} -> {String.Join(' ', targetSequence)}");
+                    break;
+                }
+            }
         }
     }
 }
