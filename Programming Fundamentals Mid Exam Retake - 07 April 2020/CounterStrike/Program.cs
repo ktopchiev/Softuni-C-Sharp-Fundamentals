dbbdf9c679c1515f7bb5d1ab480f7b426485e30e
ktopchiev <karlostopchiev@gmail.com>
2020-11-07 09:47:41 +0200

    mid exam

---
+++ Programming Fundamentals Mid Exam Retake - 07 April 2020/CounterStrike/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace CounterStrike
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int energy = int.Parse(Console.ReadLine());
+            int wins = 0;
+            int bonusEnergy = 0;
+            
+            while (true)
+            {
+                string command = Console.ReadLine();
+
+                if (!command.Equals("End of battle"))
+                {
+                    var distance = Convert.ToInt32(command);
+
+                    if (energy - distance >= 0)
+                    {
+                        energy -= distance;
+                        wins++;
+                        if (wins % 3 == 0)
+                        {
+                            energy += wins;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Not enough energy! Game ends with {wins} won battles and {energy} energy");
+                        break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"Won battles: {wins}. Energy left: {energy}");
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ Programming Fundamentals Mid Exam Retake - 12 August 2020/TheLift/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TheLift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int peopleWait = int.Parse(Console.ReadLine());
+            
+        }
+    }
+}
