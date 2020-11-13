98d5097af5f34e71227f646f1471786a36eca789
ktopchiev <karlostopchiev@gmail.com>
2020-11-13 12:35:23 +0200

    minerTask

---
+++ AssociativeArrays/AMinerTask/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AMinerTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+            Dictionary<string, int> resources = new Dictionary<string, int>();
+            string resource = null;
+            int quantity = 0;
+            
+            while (true)
+            {
+                counter++;
+                string input = Console.ReadLine();
+
+                if (input == "stop")
+                {
+                    break;
+                }
+                
+                if (counter % 2 == 0)
+                {
+                    quantity = int.Parse(input);
+                }
+                else
+                {
+                    resource = input;
+                    quantity = 0;
+                }
+                
+                if (resources.ContainsKey(resource))
+                {
+                    resources[resource] += quantity;
+                }
+                else
+                {
+                    resources[resource] = quantity;   
+                }
+            }
+
+            foreach (KeyValuePair<string, int> pair in resources)
+            {
+                Console.WriteLine($"{pair.Key} -> {pair.Value}");
+            }
+        }
+    }
+}
