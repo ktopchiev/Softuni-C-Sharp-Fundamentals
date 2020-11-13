96e7d32754a953d4809d151406f9e39a8a534610
ktopchiev <karlostopchiev@gmail.com>
2020-11-13 12:06:21 +0200

    countChars

--- AssociativeArrays/CountCharsInAString/Program.cs
+++ AssociativeArrays/CountCharsInAString/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace CountCharsInAString
 {
@@ -6,7 +8,31 @@ namespace CountCharsInAString
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            char[] str = Console.ReadLine().ToCharArray();
+            Dictionary<char, int> counts = new Dictionary<char, int>();
+            
+            foreach (var cr in str.Where(x=>x != ' '))
+            {
+                int counter = 0;
+                
+                for (int i = 0; i < str.Length; i++)
+                {
+                    if (str[i] == cr)
+                    {
+                        counter++;
+                    }
+                }
+
+                if (!counts.ContainsKey(cr))
+                {
+                    counts.Add(cr,counter);   
+                }
+            }
+
+            foreach (KeyValuePair<char,int> pair in counts)
+            {
+                Console.WriteLine($"{pair.Key} -> {pair.Value}");
+            }
         }
     }
 }
