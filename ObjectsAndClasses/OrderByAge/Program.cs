9f030458acbd39d9435a362d008619380002c27c
ktopchiev <karlostopchiev@gmail.com>
2020-11-13 11:32:17 +0200

    vehiclecatalogue

--- ObjectsAndClasses/OrderByAge/Program.cs
+++ ObjectsAndClasses/OrderByAge/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace OrderByAge
 {
@@ -6,7 +9,51 @@ namespace OrderByAge
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Person> persons = new List<Person>();
+            
+            while (true)
+            {
+                string[] input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                if (input.Contains("End"))
+                {
+                    break;
+                }
+
+                string id = input[1];
+                int age = int.Parse(input[2]);
+                
+                Person person = new Person(input[0], id, age);
+                persons.Add(person);
+            }
+
+            var orderedPersons = persons.OrderBy(x => x.Age).ToList();
+            
+            foreach (var person in orderedPersons)
+            {
+                Console.WriteLine(person);
+            }
+        }
+    }
+
+    class Person
+    {
+        public string Name { get; set; }
+        public string ID { get; set; }
+        public int Age { get; set; }
+
+        public Person(string name, string id, int age)
+        {
+            Name = name;
+            ID = id;
+            Age = age;
+        }
+        
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{Name.First().ToString().ToUpper() + Name.Substring(1)} with ID: {ID} is {Age} years old.");
+            return sb.ToString().TrimEnd();
         }
     }
 }
