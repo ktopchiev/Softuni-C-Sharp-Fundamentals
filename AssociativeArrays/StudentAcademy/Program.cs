47ba0a9c2b125a63274fdab69d49cf5dd3e3f75e
ktopchiev <karlostopchiev@gmail.com>
2020-11-19 20:11:40 +0200

    academy

--- AssociativeArrays/StudentAcademy/Program.cs
+++ AssociativeArrays/StudentAcademy/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace StudentAcademy
 {
@@ -6,7 +8,43 @@ namespace StudentAcademy
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int rowsNum = int.Parse(Console.ReadLine());
+            Dictionary<string, List<double>> studentsAndGrades = new Dictionary<string, List<double>>();
+
+            for (int i = 0; i < rowsNum; i++)
+            {
+                string studentName = Console.ReadLine();
+                double grade = double.Parse(Console.ReadLine());
+                bool studentExist = studentsAndGrades.ContainsKey(studentName);
+                
+                if (studentExist)
+                {
+                    AddStudentAndGrade(studentsAndGrades, studentName, grade);
+                }
+                else
+                {
+                    studentsAndGrades.Add(studentName, new List<double>());
+                    AddStudentAndGrade(studentsAndGrades, studentName, grade);
+                }
+            }
+
+            foreach (var pair in studentsAndGrades)
+            {
+                var avg = pair.Value.Average();
+                pair.Value.Clear();
+                pair.Value.Add(avg);
+            }
+
+            foreach (var pair in studentsAndGrades.Where(x => x.Value[0] >= 4.50).OrderByDescending(x => x.Value[0]))
+            {
+                Console.WriteLine($"{pair.Key} -> {pair.Value[0]:f2}");
+            }
+        }
+
+        static void AddStudentAndGrade(Dictionary<string, List<double>> studentsAndGrades, string studentName, double grade)
+        {
+            var gradesList = studentsAndGrades.FirstOrDefault(x => x.Key == studentName).Value;
+            gradesList.Add(grade);
         }
     }
 }
