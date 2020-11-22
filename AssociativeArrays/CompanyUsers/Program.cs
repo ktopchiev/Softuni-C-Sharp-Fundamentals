63aa984b38f4f4e953cba31cc83063fedda00e16
ktopchiev <karlostopchiev@gmail.com>
2020-11-22 16:28:02 +0200

    text processing

--- AssociativeArrays/CompanyUsers/Program.cs
+++ AssociativeArrays/CompanyUsers/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace CompanyUsers
 {
@@ -6,7 +9,42 @@ namespace CompanyUsers
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Dictionary<string, List<string>> companies = new Dictionary<string, List<string>>();
+
+            while (true)
+            {
+                string[] input = Console.ReadLine().Split(" -> ").ToArray();
+                string companyInput = input[0];
+                
+                if (companyInput == "End")
+                {
+                    break;
+                }
+
+                string employeeId = input[1];
+                bool companyExist = companies.ContainsKey(companyInput);
+
+                if (companyExist && !companies[companyInput].Contains(employeeId))
+                {
+                    companies[companyInput].Add(employeeId);
+                }
+                else if (!companyExist)
+                {
+                    companies.Add(companyInput, new List<string>());
+                    var companyList = companies.FirstOrDefault(x => x.Key == companyInput).Value;
+                    companyList.Add(employeeId);
+                }
+            }
+
+            foreach (var pair in companies.OrderBy(x => x.Key))
+            {
+                Console.WriteLine(pair.Key);
+                
+                foreach (var employeeId in pair.Value)
+                {
+                    Console.WriteLine($"-- {employeeId}");
+                }
+            }
         }
     }
 }
--- AssociativeArrays/LegendaryFarming/Program.cs
+++ AssociativeArrays/LegendaryFarming/Program.cs
@@ -8,84 +8,18 @@ namespace LegendaryFarming
     {
         static void Main(string[] args)
         {
-            Dictionary<string,int> items = new Dictionary<string, int>();
-            items.Add("shards",0);
-            items.Add("fragments", 0);
-            items.Add("motes", 0);
-            Dictionary<string,int> junk = new Dictionary<string, int>();
-            bool end = false;
+            Dictionary<string, int> materials = new Dictionary<string, int>();
+            Dictionary<string, int> junk = new Dictionary<string, int>();
             
-            while (!end)
+            
+            
+            while (true)
             {
-                string[] input = Console.ReadLine().ToLower().Split(' ',StringSplitOptions.RemoveEmptyEntries);
+                string[] input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
 
-                for (int i = 1; i <= input.Length; i++)
+                for (int i = 0; i < input.Length; i+=2)
                 {
-                    if (i % 2 == 0)
-                    {
-                        if (input[i - 1] == "shards")
-                        {
-                            items["shards"] += int.Parse(input[i - 2]);   
-                        }
-                        else if (input[i - 1] == "fragments")
-                        {
-                            items["fragments"] += int.Parse(input[i - 2]);
-                        }
-                        else if (input[i - 1] == "motes")
-                        {
-                            items["motes"] += int.Parse(input[i - 2]);
-                        }
-                        else
-                        {
-                            if (junk.ContainsKey(input[i - 1]))
-                            {
-                                junk[input[i - 1]] = int.Parse(input[i - 2]);
-                            }
-                            else
-                            {
-                                junk.Add(input[i - 1], int.Parse(input[i - 2]));   
-                            }
-                        }
-
-                        if (items.Any(x=>x.Value >= 250))
-                        {
-                            var key = items.First(x => x.Value >= 250).Key;
-                            items[key] -= 250;
-                            string winner = null;
-                            
-                            switch (key)
-                            {
-                                case "shards":
-                                    winner = "Shadowmourne";
-                                    break;
-                                case "fragments":
-                                    winner = "Valanyr";
-                                    break;
-                                case "motes":
-                                    winner = "Dragonwrath";
-                                    break;
-                            }
-                            
-                            Console.WriteLine($"{winner} obtained!");
-
-                            foreach (var item in items.OrderByDescending(x => x.Value)
-                                .ThenBy(x => x.Key))
-                            {
-                                Console.WriteLine($"{item.Key}: {item.Value}");
-                            }
-
-                            if (junk.Count != 0)
-                            {
-                                foreach (var item in junk.OrderBy(x => x.Key))
-                                {
-                                    Console.WriteLine($"{item.Key}: {item.Value}");
-                                }   
-                            }
-
-                            end = true;
-                            break;
-                        }
-                    }
+                    
                 }
             }
         }
