617521a079597e49dcbfbf18df1ed239913bba28
ktopchiev <karlostopchiev@gmail.com>
2020-11-24 00:13:50 +0200

    CaesarCipher

--- TextProcessing/CaesarCipher/Program.cs
+++ TextProcessing/CaesarCipher/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Text;
 
 namespace CaesarCipher
 {
@@ -6,7 +8,17 @@ namespace CaesarCipher
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            char[] input = Console.ReadLine().ToCharArray();
+            byte[] chars = Encoding.ASCII.GetBytes(input);
+            StringBuilder encryptedOutput = new StringBuilder();
+            
+            foreach (var ch in chars)
+            {
+                char newCh = Convert.ToChar(ch + 3);
+                encryptedOutput.Append(newCh);
+            }
+
+            Console.WriteLine(encryptedOutput);
         }
     }
 }
