c426052b56be23f99b0a352c57ccf102370dfc03
ktopchiev <karlostopchiev@gmail.com>
2020-11-23 12:30:47 +0200

    chars multiplier

---
+++ TextProcessing/CharacterMultiplier/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace CharacterMultiplier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ').ToArray();
+            long multiplier = CharacterMultiply(input[0], input[1]);
+            Console.WriteLine(multiplier);
+        }
+
+        public static long CharacterMultiply(string firstString, string secondString)
+        {
+            long sum = 0;
+            string stringOne = firstString;
+            string stringTwo = secondString;
+            string[] strings = {stringOne, stringTwo};
+            int shortestLength = strings.Min(x => x.Length);
+            int longestLength = strings.Max(x => x.Length);
+
+            for (int i = 0; i < shortestLength; i++)
+            {
+                sum += stringOne[i] * stringTwo[i];
+            }
+
+            string longestString =  strings.FirstOrDefault(x => x.Length == longestLength);
+
+            for (int i = shortestLength; i < longestLength; i++)
+            {
+                sum += longestString[i];
+            }
+            
+            return sum;
+        }
+    }
+}
