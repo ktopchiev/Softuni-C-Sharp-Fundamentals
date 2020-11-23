3c7d379da35962fb2aa8c917e0e91871fba6f4b3
ktopchiev <karlostopchiev@gmail.com>
2020-11-23 13:30:12 +0200

    extract file

---
+++ TextProcessing/ExtractFile/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace ExtractFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] filePath = Console.ReadLine().Split('\\').ToArray();
+            string[] lastFile = filePath.LastOrDefault().Split('.').ToArray();
+
+            Console.WriteLine($"File name: {lastFile[0]}");
+            Console.WriteLine($"File extension: {lastFile[1]}");
+        }
+    }
+}
