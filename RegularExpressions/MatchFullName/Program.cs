5af573ce31b348506ecf74ca73bceed3300813c2
ktopchiev <karlostopchiev@gmail.com>
2020-11-28 00:24:46 +0200

    .

---
+++ RegularExpressions/MatchFullName/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MatchFullName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
