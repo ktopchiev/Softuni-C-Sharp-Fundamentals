edd8c208a163cc4603e96453d0d85b4ac6c35084
ktopchiev <karlostopchiev@gmail.com>
2020-12-01 20:11:30 +0200

    matchDates

---
+++ RegularExpressions/MatchPhoneNumber/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace MatchPhoneNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var regex = @"([+]{1}[359]{3}[\s][0-9][\s][0-9]{3}[\s][0-9]{4}\b)|([+]{1}[359]{3}[-][0-9][-][0-9]{3}[-][0-9]{4}\b)";
+            var phones = Console.ReadLine();
+            var phoneMatches = Regex.Matches(phones, regex);
+            var matchedPhones = phoneMatches
+                .Cast<Match>()
+                .Select(x => x.Value.Trim())
+                .ToArray();
+            Console.WriteLine(string.Join(", ", matchedPhones));
+        }
+    }
+}
