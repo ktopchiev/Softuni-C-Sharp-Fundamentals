15b0e8c02ffe743f79ab0b0b84153b82e55f033d
ktopchiev <karlostopchiev@gmail.com>
2020-12-01 22:31:24 +0200

    Furniture

---
+++ RegularExpressions/Furniture/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Furniture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var regex = @"\B^\s*>>(?<furnitureName>[A-z]+)<<(?<price>\d*\.?\d*)!(?<quantity>\d*)\b";
+            
+            List<string> purchasedFurnitures = new List<string>();
+                
+            double totalPrice = 0.0;
+            
+            while (true)
+            {
+                var input = Console.ReadLine();
+
+                if (input == "Purchase")
+                {
+                    Console.WriteLine("Bought furniture:");
+                    
+                    foreach (var furniture in purchasedFurnitures)
+                    {
+                        Console.WriteLine(furniture);
+                    }
+
+                    Console.WriteLine($"Total money spend: {totalPrice:f2}");
+                    break;
+                }
+
+                MatchCollection cart = Regex.Matches(input, regex);
+
+                foreach (Match stock in cart)
+                {
+                    var furniture = stock.Groups["furnitureName"].Value;
+                    var price = double.Parse(stock.Groups["price"].Value);
+                    var quantity = double.Parse(stock.Groups["quantity"].Value);
+                    purchasedFurnitures.Add(furniture);
+                    totalPrice += quantity * price;
+                }
+            }
+        }
+    }
+}
