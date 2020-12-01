e82aa4ea782af5a0168e425a7fc6de112dea732a
ktopchiev <karlostopchiev@gmail.com>
2020-12-01 21:19:39 +0200

    matchDates

--- RegularExpressions/MatchDates/Program.cs
+++ RegularExpressions/MatchDates/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace MatchDates
 {
@@ -6,7 +8,18 @@ namespace MatchDates
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var regex = @"\b(?<day>\d{2})([-./])(?<month>[A-Z][a-z]{2})\1(?<year>\d{4})\b";
+            var input = Console.ReadLine();
+            var dates = Regex.Matches(input, regex);
+
+            foreach (Match date in dates)
+            {
+                var day = date.Groups["day"].Value;
+                var month = date.Groups["month"].Value;
+                var year = date.Groups["year"].Value;
+
+                Console.WriteLine($"Day: {day}, Month: {month}, Year: {year}");
+            }
         }
     }
 }
