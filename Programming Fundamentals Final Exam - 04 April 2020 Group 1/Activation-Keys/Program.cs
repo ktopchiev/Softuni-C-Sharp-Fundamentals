8a3719c22a6b6615ba2bf017110af5ee2a5d43d5
ktopchiev <karlostopchiev@gmail.com>
2020-12-06 18:29:39 +0200

    Activation keys solved!

--- Programming Fundamentals Final Exam - 04 April 2020 Group 1/Activation-Keys/Program.cs
+++ Programming Fundamentals Final Exam - 04 April 2020 Group 1/Activation-Keys/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace Activation_Keys
 {
@@ -6,7 +9,94 @@ namespace Activation_Keys
     {
         static void Main(string[] args)
         {
-            string 
+            string rawActivationKey = Console.ReadLine();
+
+            while (true)
+            {
+                string[] commands = Console.ReadLine().Split(">>>");
+                string command = commands[0];
+                
+                if (commands.Contains("Generate"))
+                {
+                    Console.WriteLine($"Your activation key is: {rawActivationKey}");
+                    break;
+                }
+
+                if (command.Equals("Contains"))
+                {
+                    Contains(rawActivationKey, commands[1]);
+                }
+                else if (command.Equals("Flip"))
+                {
+                    string substrCase = commands[1];
+                    int startIndex = int.Parse(commands[2]);
+                    int endIndex = int.Parse(commands[3]);
+
+                    if (substrCase.Equals("Upper"))
+                        rawActivationKey = Upper(rawActivationKey, startIndex, endIndex);
+                    else
+                        rawActivationKey = Lower(rawActivationKey, startIndex, endIndex);
+
+                    Console.WriteLine(rawActivationKey);
+                }
+                else if (command.Equals("Slice"))
+                {
+                    int startIndex = int.Parse(commands[1]);
+                    int endIndex = int.Parse(commands[2]);
+
+                    rawActivationKey = Slice(rawActivationKey, startIndex, endIndex);
+
+                    Console.WriteLine(rawActivationKey);
+                }
+                
+            }
+        }
+
+        public static void Contains(string key, string substring)
+        {
+            if (key.Contains(substring))
+                Console.WriteLine($"{key} contains {substring}");
+            else
+                Console.WriteLine("Substring not found!");
+        }
+
+        public static string Upper(string key, int startIndex, int endIndex)
+        {
+            char[] rakArr = key.ToCharArray();
+            for (int i = startIndex; i < endIndex; i++)
+            {
+                rakArr[i] = Char.ToUpper(rakArr[i]);
+            }
+
+            key = string.Join("", rakArr);
+
+            return key;
+        }
+
+        public static string Lower(string key, int startIndex, int endIndex)
+        {
+            char[] rakArr = key.ToCharArray();
+            for (int i = startIndex; i < endIndex; i++)
+            {
+                rakArr[i] = Char.ToLower(rakArr[i]);
+            }
+
+            key = string.Join("", rakArr);
+
+            return key;
+        }
+
+        public static string Slice(string key, int startIndex, int endIndex)
+        {
+            List<char> keyArr = key.ToList();
+            
+            var range = endIndex - startIndex;
+            
+            keyArr.RemoveRange(startIndex, range);
+
+            key = string.Join("", keyArr);
+            
+            return key;
         }
     }
 }
