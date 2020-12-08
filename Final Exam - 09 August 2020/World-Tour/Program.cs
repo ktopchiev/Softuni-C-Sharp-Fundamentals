a8cd8a51ff88da0231df8d3749c1140e1aff2ed4
ktopchiev <karlostopchiev@gmail.com>
2020-12-08 11:49:25 +0200

    final exam 09 august

---
+++ Final Exam - 09 August 2020/World-Tour/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace World_Tour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
