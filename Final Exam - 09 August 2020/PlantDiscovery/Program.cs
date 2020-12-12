04a49f18979ba87810fec82d30db9656908ba0c3
ktopchiev <karlostopchiev@gmail.com>
2020-12-12 12:28:53 +0200

    plant discovery

---
+++ Final Exam - 09 August 2020/PlantDiscovery/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PlantDiscovery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Dictionary<string, List<double>> plantsData = new Dictionary<string, List<double>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] plantsInitialInput = Console.ReadLine().Split("<->");
+                string plantName = plantsInitialInput[0];
+                double plantRarity = double.Parse(plantsInitialInput[1]);
+                List<double> plantDataList = new List<double>(){plantRarity};
+                
+                if (plantsData.ContainsKey(plantName))
+                {
+                    var plantTempList = plantsData[plantName];
+                    plantTempList[0] = plantRarity;
+                }
+                
+                plantsData.Add(plantName, plantDataList);
+            }
+
+            while (true)
+            {
+                string[] commands = Console.ReadLine().Split();
+                string command = commands[0];
+
+                if (command == "Exhibition")
+                {
+                    Console.WriteLine("Plants for the exhibition:");
+                    plantsData.OrderByDescending(x => x.Value[0]);
+
+                    foreach (var plant in plantsData)
+                    {
+                        var plantData = plant.Value;
+                        var plantRarity = plantData[0];
+                        plantData.RemoveAt(0);
+                        var plantAvgRating = 0.0;
+                        if (plantData.Count > 0)
+                        {
+                            plantAvgRating = plantData.Average();   
+                        }
+                        plantData.Clear();
+                        plantData.Add(plantRarity);
+                        plantData.Add(plantAvgRating);
+                    }
+                    
+                    foreach (var plant in plantsData.OrderByDescending(x => x.Value[0]).ThenByDescending(x => x.Value[1]))
+                    {
+                        var plantData = plant.Value;
+                        var plantRarity = plantData[0];
+                        var plantRating = plantData[1];
+                        Console.WriteLine($"- {plant.Key}; Rarity: {plantRarity}; Rating: {plantRating:f2}");
+                    }
+                    break;
+                }
+                
+                if (!plantsData.ContainsKey(commands[1]))
+                {
+                    Console.WriteLine("error");
+                    continue;
+                }
+                
+                if (command == "Rate:")
+                {
+                    string plant = commands[1];
+                    int rating = int.Parse(commands[3]);
+
+                    var plantListOfData = plantsData[plant];
+                    plantListOfData.Add(rating);
+                }
+                else if (command == "Update:")
+                {
+                    string plant = commands[1];
+                    int newRarity = int.Parse(commands[3]);
+
+                    var plantListOfData = plantsData[plant];
+                    plantListOfData.RemoveAt(0);
+                    plantListOfData.Insert(0, newRarity);
+                }
+                else if (command == "Reset:")
+                {
+                    string plant = commands[1];
+                    var plantsListOfData = plantsData[plant];
+                    plantsListOfData.RemoveRange(1, plantsListOfData.Count - 1);
+                }
+                else
+                {
+                    Console.WriteLine("error");
+                }
+            }
+        }
+    }
+}
--- Final Exam - 09 August 2020/World-Tour/Program.cs
+++ Final Exam - 09 August 2020/World-Tour/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace World_Tour
 {
@@ -6,7 +8,73 @@ namespace World_Tour
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<char> stops = Console.ReadLine().ToList();
+
+            while (true)
+            {
+                string[] commands = Console.ReadLine().Split(":");
+                string command = commands[0];
+                
+                if (command == "Travel")
+                {
+                    Console.WriteLine($"Ready for world tour! Planned stops: {string.Join("", stops).Trim()}");
+                    break;
+                }
+
+                if (command == "Add Stop")
+                {
+                    int index = int.Parse(commands[1]);
+                    string str = commands[2];
+
+                    if (index < stops.Count && index >= 0)
+                    {
+                        List<char> stringForInsert = str.ToList();
+                        stops.InsertRange(index, stringForInsert);
+                    }
+
+                    Console.WriteLine(string.Join("", stops));
+                }
+                else if (command == "Remove Stop")
+                {
+                    int startIndex = int.Parse(commands[1]);
+                    int endIndex = int.Parse(commands[2]);
+
+                    if (startIndex < stops.Count && startIndex >= 0 && endIndex < stops.Count && endIndex >= 0)
+                    {
+                        if (endIndex >= startIndex)
+                        {
+                            stops.RemoveRange(startIndex, endIndex - startIndex + 1);
+                        }
+                    }
+                    
+                    Console.WriteLine(string.Join("", stops));
+                }
+                else if (command == "Switch")
+                {
+                    
+                    string oldString = commands[1];
+                    string newString = commands[2];
+                    string stopsStr = string.Join("", stops);
+
+                    if (stopsStr.Contains(oldString))
+                    {
+                        while(stopsStr.Contains(oldString))
+                        {
+                        
+                            int startSwitchIndex = stopsStr.IndexOf(oldString);
+                            char[] newStringArr = newString.ToCharArray();
+                            stops.InsertRange(startSwitchIndex, newStringArr);
+                            int oldStrlen = oldString.Length;
+                            int newStrLastIndex = startSwitchIndex + newString.Length;
+                            stops.RemoveRange(newStrLastIndex, oldStrlen);
+                            stopsStr = string.Join("", stops);
+                        }
+                        
+                    }
+                    
+                    Console.WriteLine(string.Join("", stops));
+                }
+            }
         }
     }
 }
